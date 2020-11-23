90c855852d5d57f7fd8e978dced31ba8e9316293
Alena Katkova <katkova.web@gmail.com>
2020-11-23 18:06:40 +0100

    1295 - solution with dividing by 10

--- 1295_Find_Numbers_with_Even_Number_of_Digits.js
+++ 1295_Find_Numbers_with_Even_Number_of_Digits.js
@@ -5,7 +5,15 @@
 var findNumbers = function(nums) {
   let count = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i].toString().length % 2 === 0) count++;
+    let num = nums[i];
+    let numberOfDigits = 0;
+    while (num >= 1)
+    {
+      num /= 10;
+      numberOfDigits++;
+    }
+
+    if (numberOfDigits % 2 === 0) count++;
   }
   return count;
 };
