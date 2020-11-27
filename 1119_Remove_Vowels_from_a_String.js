b44399bbed68e0666c07632d56aa56606e161748
Alena Katkova <katkova.web@gmail.com>
2020-11-27 18:02:30 +0100

    1119 - string replace solution

--- 1119_Remove_Vowels_from_a_String.js
+++ 1119_Remove_Vowels_from_a_String.js
@@ -3,21 +3,5 @@
  * @return {string}
  */
 var removeVowels = function(S) {
-  const vowels = {
-    a: true,
-    e: true,
-    i: true,
-    o: true,
-    u: true
-  };
-
-  let result = "";
-
-  for (let i = 0; i < S.length; i++) {
-    if (!vowels[S[i]]) {
-      result += S[i];
-    }
-  }
-
-  return result;
+  return S.replace(/a|e|i|o|u/g, "");
 };
