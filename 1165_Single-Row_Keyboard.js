341a1c8b44a2ae5be5e4e4e18ea5fa0d326561a0
Alena Katkova <katkova.web@gmail.com>
2020-11-27 18:50:14 +0100

    1165 - hash table solution

--- 1165_Single-Row_Keyboard.js
+++ 1165_Single-Row_Keyboard.js
@@ -7,9 +7,14 @@ var calculateTime = function(keyboard, word) {
   let time = 0;
   let previous = 0;
   let next = 0;
+  let keyboardIndices = {};
+
+  for (let i = 0; i < keyboard.length; i++) {
+    keyboardIndices[keyboard[i]] = i;
+  }
 
   for (let i = 0; i < word.length; i++) {
-    next = keyboard.indexOf(word[i]);
+    next = keyboardIndices[word[i]];
     time += Math.abs(next - previous);
     previous = next;
   }
