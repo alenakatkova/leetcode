98c0176241e963a7d07551e0ea0db3c31e40670a
Alena Katkova <katkova.web@gmail.com>
2020-11-27 18:24:45 +0100

    760 - brute force

---
+++ 760_Find_Anagram_Mappings.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number[]}
+ */
+var anagramMappings = function(A, B) {
+  let result = [];
+
+  for (let i = 0; i < A.length; i++) {
+    result.push(B.indexOf(A[i]));
+  }
+
+  return result;
+};
