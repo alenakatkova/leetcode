ac862b1b40e3b45b43aa81d19811106f281df1f9
Alena Katkova <katkova.web@gmail.com>
2020-12-04 17:32:49 +0100

    1221 - two variables solution

--- 1221_Split_a_String_in_Balanced_Strings.js
+++ 1221_Split_a_String_in_Balanced_Strings.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 const balancedStringSplit = (s) => {
-  let rCount = 0;
-  let lCount = 0;
+  let balance = 0;
   let amountOfBalancedStrings = 0;
 
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === "R") rCount++;
-    else lCount++;
+    if (s[i] === "R") balance++;
+    else balance--;
 
-    if (rCount !== 0 && rCount === lCount) amountOfBalancedStrings++;
+    if (balance === 0) amountOfBalancedStrings++;
   }
 
   return amountOfBalancedStrings;
