0ea8b52674e0a20dbc469bbf5060adc0c0975c48
Alena Katkova <katkova.web@gmail.com>
2020-12-09 23:28:27 +0100

    136 - xor solution

--- 136_Single_Number.js
+++ 136_Single_Number.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-      let counter = {};
-      for (let i = 0; i < nums.length; i++) {
-        counter[nums[i]] = !counter.hasOwnProperty(nums[i]);
-      }
-      return Object.keys(counter).find(key => counter[key] === true);
-    };
+  let result = 0;
+  for (i = 0; i < nums.length; i++) {
+    result ^= nums[i];
+  }
+  return result;
+};
