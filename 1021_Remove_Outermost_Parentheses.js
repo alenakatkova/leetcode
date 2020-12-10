03e5055c88f223de03462aa020a7b7b3858e3f41
Alena Katkova <katkova.web@gmail.com>
2020-12-10 14:36:57 +0100

    1021 - counter solution (less memory)

--- 1021_Remove_Outermost_Parentheses.js
+++ 1021_Remove_Outermost_Parentheses.js
@@ -3,18 +3,18 @@
  * @return {string}
  */
 var removeOuterParentheses = function(S) {
-  let stack = [];
   let lastOuterPosition = 0;
+  let notClosedCounter = 0;
   let result = "";
 
   for (let i = 0; i < S.length; i++) {
-    if (S[i] === ")") stack.pop();
-    else stack.push(S[i]);
+    if (S[i] === "(") notClosedCounter++;
+    else notClosedCounter--;
 
-    if (stack.length === 0) {
+    if (notClosedCounter === 0) {
       result += S.slice(lastOuterPosition + 1, i);
-      lastOuterPosition = i + 1;
-    }
+      lastOuterPosition = i + 1; // + 1 because we have two outer parantheses one after another
+    };
   }
 
   return result;
