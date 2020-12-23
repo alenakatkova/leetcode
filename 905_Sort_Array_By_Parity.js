41922cc6e8c3a54dc1b97932cd64831cff4448a2
Alena Katkova <katkova.web@gmail.com>
2020-12-23 16:04:13 +0100

    905 - two indices solution

--- 905_Sort_Array_By_Parity.js
+++ 905_Sort_Array_By_Parity.js
@@ -3,11 +3,17 @@
  * @return {number[]}
  */
 const sortArrayByParity = (A) => {
-  let odds = [];
-  let evens = [];
-  for (let i = 0; i < A.length; i++) {
-    if (A[i] % 2 === 0) evens.push(A[i]);
-    else odds.push(A[i]);
+  let i = 0;
+  let j = A.length - 1;
+  while (i < j) {
+    if (A[i] % 2 === 1 && A[j] % 2 === 0) {
+      let tmp = A[i];
+      A[i] = A[j];
+      A[j] = tmp;
+    }
+
+    if (A[i] % 2 === 0) i++;
+    if (A[j] % 2 === 1) j--;
   }
-  return evens.concat(odds);
+  return A;
 };
