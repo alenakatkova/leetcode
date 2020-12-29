5fead31662cd7045a4eaa594752410a00f478821
Alena Katkova <katkova.web@gmail.com>
2020-12-29 18:57:37 +0100

    1299 - from the end solution

--- 1299_Replace_Elements_with_Greatest_Element_on_Right_Side.js
+++ 1299_Replace_Elements_with_Greatest_Element_on_Right_Side.js
@@ -3,20 +3,13 @@
  * @return {number[]}
  */
 const replaceElements = (arr) => {
-  let sorted = [ ...arr ].sort((a, b) => b - a);
-  let counter = arr.reduce((acc, cur) => {
-    acc[cur] = (acc[cur] || 0) + 1;
-    return acc;
-  }, {});
-  let pointer = 0;
+  let max = -1;
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    counter[arr[i]]--;
-    if (arr[i] === sorted[pointer]) pointer++;
-    while (counter[sorted[pointer]] === 0) pointer++;
-    arr[i] = sorted[pointer];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let cur = arr[i];
+    arr[i] = max;
+    max = Math.max(cur, max);
   }
 
-  arr[arr.length - 1] = -1;
   return arr;
 };
