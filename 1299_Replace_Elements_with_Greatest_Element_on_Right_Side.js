a2c17d518642ef70044b609788ff22bfa2777a8c
Alena Katkova <katkova.web@gmail.com>
2020-12-29 18:48:08 +0100

    1299 - hash, sort and pointer solution

--- 1299_Replace_Elements_with_Greatest_Element_on_Right_Side.js
+++ 1299_Replace_Elements_with_Greatest_Element_on_Right_Side.js
@@ -3,8 +3,18 @@
  * @return {number[]}
  */
 const replaceElements = (arr) => {
+  let sorted = [ ...arr ].sort((a, b) => b - a);
+  let counter = arr.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
+  let pointer = 0;
+
   for (let i = 0; i < arr.length - 1; i++) {
-    arr[i] = Math.max( ...arr.slice(i + 1) );
+    counter[arr[i]]--;
+    if (arr[i] === sorted[pointer]) pointer++;
+    while (counter[sorted[pointer]] === 0) pointer++;
+    arr[i] = sorted[pointer];
   }
 
   arr[arr.length - 1] = -1;
